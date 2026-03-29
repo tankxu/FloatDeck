ea0d3aa32ab7fbe5cceac80a2511c6272d4f53ee
Tank Xu <hellotank@qq.com>
2026-03-29 23:25:39 +0700

    Initial Commit

---
+++ FloatDeck/FloatDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatDeckApp.swift
+//  FloatDeck
+//
+//  Created by tank on 29/3/26.
+//
+
+import SwiftUI
+
+@main
+struct FloatDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
